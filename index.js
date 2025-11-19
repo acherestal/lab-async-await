5c152969ba7c40aaa06e6062bef6665e933ea123
Alanda <alandacherestal@gmail.com>
2025-11-19 11:08:17 -0500

    added code to index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your code here!
+fetch ('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts =>
+        displayPosts(posts)
+    )
+    .catch(error => 
+        {console.error('Error fetching user data:', error);})
+
+const displayPosts = (posts) => {
+    posts.forEach(post => {
+
+        const list = document.createElement('li');
+        const title = document.createElement('h1');
+        title.textContent = post.title;
+        const body = document.createElement('p');
+        body.textContent = post.body;
+
+        list.append(title, body);
+
+        const postList = document.querySelector('#post-list');
+        postList.append(list);
+    }
+    )
+}
